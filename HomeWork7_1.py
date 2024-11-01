a2608200655a0bc799521bd14c2ff0d9acc9c65c
Serhii <samarchenkoserhii@gmail.com>
2024-11-01 15:39:33 +0100

    HomeWork

--- HomeWork7_1.py
+++ HomeWork7_1.py
@@ -1,13 +1,13 @@
 
 name = "Alex"
-age = 50
+time = 50
 
 
-def say_hi(name, age):
-  return f"Hi. My name is {name} and I'm {age} years old"
+def say_hi(name, time):
+#  return f"Hi. My name is {name} and I'm {age} years old"
+  return f"Hi. My name is {name}, my experience as a programmer is {time} seconds"
 
-
-print(say_hi(name,age))
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print(say_hi(name,time))
+assert say_hi("Alex", 32) == "Hi. My name is Alex, my experience as a programmer is 32 seconds", 'Test1'
+assert say_hi("Frank", 58) == "Hi. My name is Frank, my experience as a programmer is 58 seconds", 'Test2'
 print('ОК')
