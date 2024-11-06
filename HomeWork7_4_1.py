6882731df2b08c8890f8f02a3724047f4fdac628
Serhii <samarchenkoserhii@gmail.com>
2024-11-06 22:58:38 +0100

    HomeWork

---
+++ HomeWork7_4_1.py
@@ -0,0 +1,9 @@
+List1=[]
+List2=[]
+#def common_elements():
+for i in range(1,100):
+    if i % 3 == 0 :List1.append(i)
+    if i % 5 == 0 :List2.append(i)
+print(List1)
+#print(List2)
+#assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
