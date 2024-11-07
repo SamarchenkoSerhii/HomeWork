fc9dcc06eb1e43804eb6d551fd487211d8f1230c
Serhii <samarchenkoserhii@gmail.com>
2024-11-07 01:35:35 +0100

    HomeWork

--- HomeWork9_1.py
+++ HomeWork9_1.py
@@ -1,10 +1,8 @@
-
-
-
 def popular_words (text, words):
-pass
+    dict_words = {}
+    text = text.lower()
+    for i in range(len(words)): dict_words[words[i]] = text.count(words[i])
+    return dict_words
 
-
-
-
-assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK')
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 1 }, 'Test1'
+print('OK')
