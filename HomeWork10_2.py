4f346d96dcc996777b2ce4f16561b75168e175bf
Serhii <samarchenkoserhii@gmail.com>
2024-11-11 13:38:56 +0100

    HomeWork

--- HomeWork10_2.py
+++ HomeWork10_2.py
@@ -10,4 +10,4 @@ assert first_word("don't touch it") == "don't", 'Test3'
 assert first_word(".., and so on ...") == "and", 'Test4'
 assert first_word("hi") == "hi", 'Test5'
 assert first_word("Hello.World") == "Hello", 'Test6'
-print('OK')
+print('OK.')
