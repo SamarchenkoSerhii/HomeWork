e1cbc98b54dba4e17a38c789884cb6452098560c
Serhii <samarchenkoserhii@gmail.com>
2024-11-19 15:19:40 +0100

    homework

--- HomeWork12_1.py
+++ HomeWork12_1.py
@@ -1,4 +1,16 @@
-import codecs 
-def delete_html_tags(html_file, result_file='cleaned.txt'): 
-      with codecs.open(html_file, 'r', 'utf-8') as file: 
-           html = file.read()
+import re
+def delete_html_tags(input_file, output_file='cleaned.txt'):
+    try:
+        with open(input_file, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+        cleaned_text = re.sub(r'<[^>]*>', '', html_content)
+
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(cleaned_text)
+        
+    except FileNotFoundError:
+        print(f"file '{input_file}'not found.")
+    except Exception as e:
+        print(f"error: {e}")
+
+delete_html_tags('draft.html', 'cleaned.txt')
