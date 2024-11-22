48048085a7c22a122c8ab4e2c58247aa3dd5cac0
Serhii <samarchenkoserhii@gmail.com>
2024-11-22 15:58:29 +0100

    homework

--- HomeWork13_1.py
+++ HomeWork13_1.py
@@ -8,15 +8,13 @@ class Human:
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.gender}, {self.age} лет"
 
-
 class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
 
     def __str__(self):
-        return f"{super().__str__()}, Зачетная книжка: {self.record_book}"
-
+        return f"{super().__str__()}, Залікова книжка: {self.record_book}"
 
 class Group:
     def __init__(self, number):
@@ -39,27 +37,17 @@ class Group:
 
     def __str__(self):
         all_students = "\n".join(str(student) for student in self.group)
-        return f"Группа: {self.number}\nСтуденты:\n{all_students}"
+        return f"Група: {self.number}\nСтуденти:\n{all_students}"
 
-
-# Тестирование
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
-
-# Проверяем вывод информации о группе
 print(gr)
-
-# Тесты поиска
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
 assert gr.find_student('Jobs2') is None, 'Test2'
-assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
-
-# Удаление студента
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку має повертати екземпляр'
 gr.delete_student('Taylor')
-print(gr) # Только один студент
-
-# Повторное удаление (без ошибок)
+print(gr) 
 gr.delete_student('Taylor')
